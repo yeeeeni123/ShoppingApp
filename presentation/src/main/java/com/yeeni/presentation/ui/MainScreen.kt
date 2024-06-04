516b9eddfe029a97fe48fee6cab40f7863b7d074
yeeeeni123 <chohoho@hanmail.net>
2024-06-04 13:58:37 +0900

    multi module

---
+++ presentation/src/main/java/com/yeeni/presentation/ui/MainScreen.kt
@@ -0,0 +1,112 @@
+package com.yeeni.presentation.ui
+
+import android.annotation.SuppressLint
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.yeeni.presentation.R
+import com.yeeni.presentation.theme.ShoppingTheme
+
+sealed class MainNavigationItem(var route: String, val icon: ImageVector, var name: String) {
+    object Main : MainNavigationItem("Main", Icons.Filled.Home,"Main")
+    object Category : MainNavigationItem("Category", Icons.Filled.Star,"Category")
+    object MyPage : MainNavigationItem("MyPage", Icons.Filled.AccountBox,"MyPage")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    ShoppingTheme {
+        MainScreen()
+    }
+}
+
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScreen() {
+    val scaffoldState = rememberScaffoldState()
+    val navController = rememberNavController()
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        bottomBar = {
+            MainBottomNavigationBar(navController)
+        }
+    ) {
+        MainNavigationScreen(navController)
+    }
+}
+
+@Composable
+fun MainBottomNavigationBar(navController: NavHostController) {
+
+    val bottomNavigationItems = listOf(
+        MainNavigationItem.Main,
+        MainNavigationItem.Category,
+        MainNavigationItem.MyPage
+    )
+
+    BottomNavigation {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        bottomNavigationItems.forEach { item ->
+            BottomNavigationItem(
+                icon = {Icon(item.icon, item.route)},
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+
+                        navController.graph.startDestinationRoute?.let {
+                            popUpTo(it) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+
+@Composable
+fun MainNavigationScreen(navController: NavHostController) {
+    NavHost(navController, startDestination = MainNavigationItem.Main.route) {
+        composable(MainNavigationItem.Main.route) {
+            Text("Hello Main")
+        }
+
+        composable(MainNavigationItem.Category.route) {
+            Text("Hello Category")
+        }
+
+        composable(MainNavigationItem.MyPage.route) {
+            Text("Hello MyPage")
+        }
+    }
+}

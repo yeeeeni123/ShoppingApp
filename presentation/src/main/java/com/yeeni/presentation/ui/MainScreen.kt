6a64db419e43ba0d97bf934fa85b2386fe2567f3
yeeeeni123 <chohoho@hanmail.net>
2024-06-04 15:08:04 +0900

    mainViewModel 구현

--- presentation/src/main/java/com/yeeni/presentation/ui/MainScreen.kt
+++ presentation/src/main/java/com/yeeni/presentation/ui/MainScreen.kt
@@ -4,11 +4,14 @@ import android.annotation.SuppressLint
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
@@ -19,6 +22,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -27,6 +31,7 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.yeeni.presentation.R
 import com.yeeni.presentation.theme.ShoppingTheme
+import com.yeeni.presentation.viewmodel.MainViewModel
 
 sealed class MainNavigationItem(var route: String, val icon: ImageVector, var name: String) {
     object Main : MainNavigationItem("Main", Icons.Filled.Home,"Main")
@@ -46,10 +51,14 @@ fun MainScreenPreview() {
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
+    val viewModel = hiltViewModel<MainViewModel>()
     val scaffoldState = rememberScaffoldState()
     val navController = rememberNavController()
 
     Scaffold(
+        topBar = {
+            Header(viewModel)
+        },
         scaffoldState = scaffoldState,
         bottomBar = {
             MainBottomNavigationBar(navController)
@@ -57,6 +66,20 @@ fun MainScreen() {
     ) {
         MainNavigationScreen(navController)
     }
+}
+
+@Composable
+fun Header(viewModel: MainViewModel) {
+    TopAppBar(
+        title = { Text(text = "Shopping")},
+        actions = {
+            IconButton(onClick = {
+                viewModel.openSearchForm()
+            }) {
+                Icon(Icons.Filled.Search, "SearchIcon")
+            }
+        }
+    )
 }
 
 @Composable
---
+++ presentation/src/main/java/com/yeeni/presentation/viewmodel/MainViewModel.kt
@@ -0,0 +1,12 @@
+package com.yeeni.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(): ViewModel() {
+    fun openSearchForm() {
+        println("openSearchForm")
+    }
+}
